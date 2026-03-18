a8cde313c4d59d1385c45fdb5cf830936a88be69
DarkblooM-IO <darkbloom.io@proton.me>
2026-03-18 09:19:31 +0100

    Wed Mar 18 09:19:31 AM CET 2026

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+import pugPlugin from "@11ty/eleventy-plugin-pug"
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pugPlugin, {
+    pretty: true
+  })
+
+  eleventyConfig.addPassthroughCopy("./src/style/")
+  eleventyConfig.addPassthroughCopy("./src/js/")
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  }
+}
