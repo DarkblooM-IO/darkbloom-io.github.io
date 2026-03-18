8880415af0c9a31733ce94cd4fbaebe2635c2856
DarkblooM-IO <darkbloom.io@proton.me>
2026-03-18 22:39:47 +0100

    Wed Mar 18 10:39:47 PM CET 2026

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,39 @@
+import mermaid from 'mermaid'
 import pugPlugin from "@11ty/eleventy-plugin-pug"
+import { execSync } from 'child_process'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import { fileURLToPath } from 'url'
 
 export default function (eleventyConfig) {
+  // Mermaid config
+  mermaid.initialize({ startOnLoad: false })
+
+  // Pug config
   eleventyConfig.addPlugin(pugPlugin, {
-    pretty: true
+    pretty: true,
+    filters: {
+      mermaid: function (text) {
+        const tmpInput = path.join(os.tmpdir(), `mermaid-${Date.now()}.mmd`)
+        const tmpOutput = path.join(os.tmpdir(), `mermaid-${Date.now()}.svg`)
+        fs.writeFileSync(tmpInput, text)
+        execSync(`mmdc -i "${tmpInput}" -o "${tmpOutput}"`)
+        const svg = fs.readFileSync(tmpOutput, 'utf8')
+        fs.unlinkSync(tmpInput)
+        fs.unlinkSync(tmpOutput)
+        return svg
+      }
+    }
   })
 
+  // Site files
   eleventyConfig.addPassthroughCopy("./src/style/")
   eleventyConfig.addPassthroughCopy("./src/js/")
   eleventyConfig.addPassthroughCopy("./src/img/")
   eleventyConfig.addPassthroughCopy("./src/favicon.png")
 
+  // 11ty config
   return {
     dir: {
       input: "src",
--- docs/js/main.js
+++ docs/js/main.js
@@ -4,3 +4,10 @@ document.querySelector("footer #date").innerText = new Date().getFullYear()
 // Bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Theme toggler
+function toggleTheme() {
+  const root = document.querySelector("html")
+  const theme = root.getAttribute("data-bs-theme")
+  root.setAttribute("data-bs-theme", theme == "dark" ? "light" : "dark")
+}
