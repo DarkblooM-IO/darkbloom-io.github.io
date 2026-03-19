4e16b466b3d83b965c82966f3217f2ebeb262a22
DarkblooM-IO <darkbloom.io@proton.me>
2026-03-19 11:05:05 +0100

    Thu Mar 19 11:05:05 AM CET 2026

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,3 +4,6 @@ document.querySelector("footer #date").innerText = new Date().getFullYear()
 // Bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Make non-internal links open in new tab
+document.querySelectorAll('a[href^="http"]').forEach((el) => el.setAttribute("target", "_blank"))
