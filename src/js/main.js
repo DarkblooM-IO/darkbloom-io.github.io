41ee004f0f8444f3e6f907890e4f7c24b149e9b3
DarkblooM-IO <darkbloom.io@proton.me>
2026-03-19 14:06:52 +0100

    Thu Mar 19 02:06:52 PM CET 2026

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,8 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 // Make non-internal links open in new tab
 document.querySelectorAll('a[href^="http"]').forEach((el) => el.setAttribute("target", "_blank"))
+
+function toggleTheme() {
+  const root = document.querySelector("html")
+  root.setAttribute("data-bs-theme", root.getAttribute("data-bs-theme") == "dark" ? "light" : "dark")
+}
